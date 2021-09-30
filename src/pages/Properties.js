627a620fa01be29ce35f086ed91ef3745a03c0b9
Ricardo A. Natal Albelo <ricardo.natal@upr.edu>
2021-09-30 17:07:21 -0400

    Setup Done

---
+++ src/pages/Properties.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Properties() {
+    return (
+        <div>
+            hello from properties page
+        </div>
+    )
+}
+
+export default Properties
---
+++ src/pages/SingleProperty.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleProperty extends Component {
+    render() {
+        return (
+            <div>
+                hello from single property page
+            </div>
+        )
+    }
+}
