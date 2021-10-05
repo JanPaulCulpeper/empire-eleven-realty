05ca8b15a2f3bea24d15a6a355f7ef1a1609e45c
JanPaulCulpeper <jan.culpeper1@upr.edu>
2021-10-05 16:51:24 -0400

    Search In Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import Services from '../components/Services'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import FeatureProperties from '../components/FeatureProperties'
 import PropertiesContainer from '../components/PropertiesContainer'
 import About from '../components/About'
@@ -13,28 +13,26 @@ import SearchContainer from '../components/SearchContainer'
 const Home = () => {
     return (
         <>
-        
-        <Hero> 
-            <Banner title="Explore with confidence" subtitle="Elite company dedicated to the sale and rental of residential, commercial and industrial properties.">
-                
-                <Link to='/properties' className="btn-primary">
-                    Properties
-                </Link>
-            </Banner>
-            
-        </Hero>
-        <SearchContainer />
-            <Link to='/properties' className="btn-primary">
-                    Search
-            </Link>
-            
-        <FeatureProperties/>
-        <Services/>
-        <About className='about' title="About" id="about"/>
-        <Reviews className='reviews' title="Reviews" id="reviews"/>
-        {/* <Foo /> */}
-        </>
-    );
+
+            <Hero>
+                <Banner title="Explore with confidence" subtitle="Elite company dedicated to the sale and rental of residential, commercial and industrial properties.">
+                <SearchContainer className="search-container">
+               
+               </SearchContainer>
+                    <Link to='/properties' className="btn-primary">
+                        Search
+                    </Link>
+                </Banner>
+
+            </Hero>
+     
+                <FeatureProperties />
+                <Services />
+                <About className='about' title="About" id="about" />
+                <Reviews className='reviews' title="Reviews" id="reviews" />
+                {/* <Foo /> */}
+            </>
+            );
 };
 
-export default Home
+            export default Home
